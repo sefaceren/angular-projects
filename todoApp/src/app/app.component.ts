8a75ff6a4e3037dc6ee6040c90b1d5667aa35b85
sefaceren <h.sefaceren@gmail.com>
2021-05-11 01:24:38 +0300

    Lesson3: Model Binding

--- todoApp/src/app/app.component.ts
+++ todoApp/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'sefa';
+  items = [
+    { description : "Kahvaltı", action:"No"},
+    { description : "Sinema", action:"No"},
+    { description : "Spor", action:"No"},
+    { description : "Ders", action:"No"}
+  ];
 }
